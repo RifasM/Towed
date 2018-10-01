76ed27e45d543d4392fa17c85dc5727eb85f4af3
RifasM <39994235+RifasM@users.noreply.github.com>
2018-10-01 18:26:28 +0530

    Add files via upload

---
+++ IP_Project_MARV/src/ip_project_marv/Class12_IP_Project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ip_project_marv;
+
+/**
+ *
+ * @author student17
+ */
+public class Class12_IP_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
